b14be6862b6150c611513d43a59ce092c08c5541
Florian Feurstein <flofeurstein@gmail.com>
2015-12-28 06:54:47 +0100

    added scope via dependency injection

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('EspIO', []);
 
-app.controller('EspIOController', ['$http', function($http, $scope){
+app.controller('EspIOController', ['$http', '$scope', function($http, $scope){
   $scope.ios=['io1', 'io2', 'io3'];
 }]);
 
